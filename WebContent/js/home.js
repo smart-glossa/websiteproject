1346281da69269f1cf75885314d58779ab313f8c
parameswaris <parameswari@smartglossa.com>
2016-11-09 14:26:41 -0800

    change error and add js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -1,295 +1,323 @@
 function board(){
-	var b6=document.createElement("section");
-     document.body.appendChild(b6);
 
+    var sec=document.createElement("section");
+    document.body.appendChild(sec);
+
+  var a=document.createElement("div");
+  a.setAttribute("id","pen");
+    sec.appendChild(a);
+  
+
+   var aa=document.createElement("p");
+  aa.setAttribute("class","pen1");
+  a.appendChild(aa);
+  var aa1=document.createTextNode("envato");
+  aa.appendChild(aa1);
+  var aa2=document.createElement("span");
+  aa.appendChild(aa2);
+  var aa3=document.createTextNode("market");
+  aa2.appendChild(aa3);
+
+  var aa4=document.createElement("p");
+  aa4.setAttribute("class","pen2");
+  a.appendChild(aa4);
+  var aa5=document.createTextNode("Buy Now");
+  aa4.appendChild(aa5);
+
+  var aa6=document.createElement("div");
+  aa6.setAttribute("class","sub");
+  sec.appendChild(aa6);
+  var aa7=document.createElement("h1");
+  aa7.setAttribute("class","pen3");
+  aa6.appendChild(aa7);
+  var aa8=document.createTextNode("THE BOARD");
+  aa7.appendChild(aa8);
+  var aa9=document.createElement("h2");
+  aa9.setAttribute("class","pen4");
+  aa6.appendChild(aa9);
+  var bb=document.createTextNode("YOUR SIMPLE ELEGANT THEME");
+  aa6.appendChild(bb);
+
+    var li=document.createElement("ul");
+    li.setAttribute("class","lill");
+    sec.appendChild(li);
+
+    var li1=document.createElement("li");
+    li1.setAttribute("class","list");
+    li.appendChild(li1);
+    var li2=document.createElement("a");
+    li2.setAttribute("href","");
+    li2.setAttribute("class","link");
+    li1.appendChild(li2);
+    var li3=document.createTextNode("HOME");
+    li2.appendChild(li3);
+
+    var li4=document.createElement("li");
+    li4.setAttribute("class","list");
+    li.appendChild(li4);
+    var li5=document.createElement("a");
+    li5.setAttribute("href","");
+    li5.setAttribute("class","link");
+    li4.appendChild(li5);
+    var li6=document.createTextNode("ABOUT US");
+    li5.appendChild(li6);
+
+    var li7=document.createElement("li");
+    li7.setAttribute("class","list");
+    li.appendChild(li7);
+    var li8=document.createElement("a");
+    li8.setAttribute("href","");
+    li8.setAttribute("class","link");
+    li7.appendChild(li8);
+    var li9=document.createTextNode("WORK");
+    li8.appendChild(li9);
+
+    var lis1=document.createElement("li");
+    lis1.setAttribute("class","list");
+    li.appendChild(lis1);
+    var lis2=document.createElement("a");
+    lis2.setAttribute("href","");
+    lis2.setAttribute("class","link");
+    lis1.appendChild(lis2);
+    var lis3=document.createTextNode("OFFERS");
+    lis2.appendChild(lis3);
+
+    var lis4=document.createElement("li");
+    lis4.setAttribute("class","list");
+    li.appendChild(lis4);
+    var lis5=document.createElement("a");
+    lis5.setAttribute("href","");
+    lis5.setAttribute("class","link");
+    lis4.appendChild(lis5);
+    var lis6=document.createTextNode("NEWS");
+    lis5.appendChild(lis6);
+    
+
+    var lis7=document.createElement("li");
+    lis7.setAttribute("class","list");
+    li.appendChild(lis7);
+    var lis8=document.createElement("a");
+    lis8.setAttribute("href","");
+    lis8.setAttribute("class","link");
+    lis7.appendChild(lis8);
+    var lis9=document.createTextNode("BLOG");
+    lis8.appendChild(lis9);
+
+    var lis10=document.createElement("li");
+    lis10.setAttribute("class","list");
+    li.appendChild(lis10);
+    var lis11=document.createElement("a");
+    lis11.setAttribute("href","");
+    lis11.setAttribute("class","link");
+    lis10.appendChild(lis11);
+    var lis12=document.createTextNode("GET IN TOUCH");
+    lis11.appendChild(lis12);
+
+
+
+     var slide=document.createElement("div");
+    slide.setAttribute("id","slideshow");
+    sec.appendChild(slide);
+
+    var slide1=document.createElement("div");
+    var slide2=document.createElement("img");
+    slide2.setAttribute("src","../images/ta.jpg");
+    slide1.appendChild(slide2);
+    slide.appendChild(slide1);
+
+    var slide3=document.createElement("div");
+    var slide4=document.createElement("img");
+    slide4.setAttribute("src","../images/t1.jpg");
+    slide3.appendChild(slide4);
+    slide.appendChild(slide3);
+
+var slide5=document.createElement("div");
+    var slide6=document.createElement("img");
+    slide6.setAttribute("src","../images/t2.jpg");
+    slide5.appendChild(slide6);
+    slide.appendChild(slide5);
+
+
+  
+
+
+
+  var sec2=document.createElement("section");
+
+    sec2.setAttribute("class","baaa");
+    document.body.appendChild(sec2);
+
+    var cc=document.createElement("h1");
+    cc.setAttribute("class","ba11");
+    sec2.appendChild(cc);
+    var cc1=document.createTextNode("ABOUT US");
+    cc.appendChild(cc1);
+
+    var cc2=document.createElement("p");
+    cc2.setAttribute("class","ba33");
+    sec2.appendChild(cc2);
+    var cc3=document.createTextNode("This is photoshop's version of lorem ipsum");
+    cc2.appendChild(cc3);
+
+    var cc4=document.createElement("p");
+    cc4.setAttribute("class","ba33");
+    sec2.appendChild(cc4);
+    var cc5=document.createTextNode("proin gravida nibh vel velt auctor,Aenean sollictiudn,loern quis bibendum aucto.");
+    cc4.appendChild(cc5);
+
+    var note=document.createElement("div");
+    note.setAttribute("class","work0");
+    sec2.appendChild(note);
+
+    var note1=document.createElement("img");
+    note1.setAttribute("src","image/4.png");
+    note1.setAttribute("class","work11");
+    note.appendChild(note1);
+
+   
+
+    var note2=document.createElement("h1");
+    note.appendChild(note2);
+    var note3=document.createTextNode("WHO WE ARE");
+    note2.appendChild(note3); 
+      
+    var note4=document.createElement("p");
+    note4.setAttribute("class","work44");
+    note.appendChild(note4);
+    var note5=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium"); 
+    note4.appendChild(note5);
+
+
+    var note6=document.createElement("div");
+    note6.setAttribute("class","work0");
+    sec2.appendChild(note6);
+
+    var note7=document.createElement("img");
+    note7.setAttribute("src","image/5.png");
+    note7.setAttribute("class","work11");
+    note6.appendChild(note7);
+
+    var note8=document.createElement("h1");
+    note6.appendChild(note8);
+    var note9=document.createTextNode("OUR GOAL");
+    note6.appendChild(note9); 
+
+    var note11=document.createElement("p");
+    note11.setAttribute("class","work44");
+    note6.appendChild(note11);
+    var note12=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium"); 
+    note6.appendChild(note12);
+
+    var top=document.createElement("div");
+    top.setAttribute("class","work0");
+    sec2.appendChild(top);
+
+    var top1=document.createElement("img");
+    top1.setAttribute("src","image/7.png");
+    top1.setAttribute("class","work11");
+    top.appendChild(top1);
+
+    var top2=document.createElement("h1");
+    top.appendChild(top2);
+    var top3=document.createTextNode("OUR PHILOSOPHY");
+    top2.appendChild(top3); 
+
+    var top4=document.createElement("p");
+    top4.setAttribute("class","work44");
+    top.appendChild(top4);
+    var top5=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium"); 
+    top4.appendChild(top5);
+
+
+    var top6=document.createElement("h1");
+    top6.setAttribute("class","lab0");
+    sec.appendChild(top6);
+    var top7=document.createTextNode("OUR SERVICES");
+    top6.appendChild(top7);
+
+    var top8=document.createElement("button");
+    top8.setAttribute("class","la11");
+    sec.appendChild(top8);
+    var top9=document.createElement("a");
+    top9.setAttribute("href","");
+    top9.setAttribute("class","lab22");
+    top8.appendChild(top9);
+    var top11=document.createTextNode("CREATIVE IDEAS");
+    top8.appendChild(top11);
+
+
+    var but=document.createElement("button");
+    but.setAttribute("class","la11");
+  sec2.appendChild(but);
+
+    var but1=document.createElement("a");
+    but1.setAttribute("href","");
+    but1.setAttribute("class","lab22");
+    but.appendChild(but1);
+    var but2=document.createTextNode("QUICK SOLUTIONS");
+    but1.appendChild(but2);
+
+
+    var but3=document.createElement("button");
+    but3.setAttribute("class","la11");
+    sec2.appendChild(but3);
+    var but4=document.createElement("a");
+    but4.setAttribute("href","");
+    but4.setAttribute("class","lab22");
+    but3.appendChild(but4);
+    var but5=document.createTextNode("MARKETING");
+    but3.appendChild(but5);
+
+    var but6=document.createElement("button");
+    but6.setAttribute("class","la11");
+    sec2.appendChild(but6);
+    var but7=document.createElement("a");
+    but7.setAttribute("href","");
+    but7.setAttribute("class","lab22");
+    but6.appendChild(but7);
+    var but8=document.createTextNode("24/7 support");
+    but6.appendChild(but8);
+
+
+    var milk=document.createElement("div");
+    milk.setAttribute("class","bala0");
+    sec2.appendChild(milk);
+    var milk1=document.createElement("h1");
+    milk1.setAttribute("class","bala11");
+    milk.appendChild(milk1);
+    var milk2=document.createTextNode("CREATIVE IDEAS");
+    milk1.appendChild(milk2);
+
+    var milk3=document.createElement("p");
+    milk3.setAttribute("class","bala33");
+    milk.appendChild(milk3);
+    var milk4=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium");
+    milk3.appendChild(milk4);
+
+    var milk5=document.createElement("p");
+    milk5.setAttribute("class","bala33");
+    milk.appendChild(milk5);
+    var milk6=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium");
+    milk5.appendChild(milk6);
+
+    var milk7=document.createElement("h1");
+  milk7.setAttribute("class","bha0");
+  sec2.appendChild(milk7);
+    var milk8=document.createTextNode("MEET THE TEAM");
+    milk7.appendChild(milk8);
+
+
+ 
 
-     var tab=document.createElement("div");
-     tab.setAttribute("class","tab2");
-     b6.appendChild(tab);
 
-	var a=document.createElement("div");
-	a.setAttribute("class","aa")
-	tab.appendChild(a);
 
 
-	var b=document.createElement("div");
-	b.setAttribute("class","aa1");
-	a.appendChild(b);
-
-
-
-	var c=document.createElement("img");
-	c.setAttribute("src","E:/sp/Board/1.svg");
-	c.setAttribute("class","aa2");
-	b.appendChild(c);
-
-	var d=document.createElement("button");
-	d.setAttribute("class","aa3");
-	var e=document.createTextNode("Buy Now");
-	d.appendChild(e);
-	b.appendChild(d);
-
-	var e=document.createElement("div");
-	a.appendChild(e);
-	e.setAttribute("class","aa4");
-
-
-	var f=document.createElement("div");
-	f.setAttribute("class","aa5")
-	e.appendChild(f);
-	var g=document.createElement("p");
-	g.setAttribute("class","aa6");
-
-   var k=document.createTextNode("THE BOARD");
-   g.appendChild(k);
-   f.appendChild(g);
-   var m=document.createElement("p");
-   m.setAttribute("class","aa7");
-  var l=document.createTextNode("YOUR SIMPLE ELGANT THEME");
-  m.appendChild(l);
-  f.appendChild(m);
-
-
-  var n=document.createElement("div");
-  n.setAttribute("class","aa8");
- tab.appendChild(n);
-
-  var o=document.createElement("ul");
-  o.setAttribute("class","aa9");
-  n.appendChild(o);
-
-
-  var p=document.createElement("li");
-  p.setAttribute("class","bb");
-  var q=document.createElement("h3");
-  var r=document.createTextNode("GET IN TOUCH");
-  p.appendChild(q);
-  q.appendChild(r);
-  o.appendChild(p);
-
-  var s=document.createElement("li");
-  s.setAttribute("class","bb");
-  var t=document.createElement("h3");
-  var u=document.createTextNode("BLOG");
-  s.appendChild(t);
-  t.appendChild(u);
-  o.appendChild(s);
-
-
-
-  var v=document.createElement("li");
-  v.setAttribute("class","bb");
-  var w=document.createElement("h3");
-  var x=document.createTextNode("NEWS");
-
-  v.appendChild(w);
-  w.appendChild(x);
-  o.appendChild(v);
-
-
-
-
-
-  var y=document.createElement("li");
-  y.setAttribute("class","bb");
-  var z=document.createElement("h3");
-  var a1=document.createTextNode("OFFERS");
-  y.appendChild(z);
-  z.appendChild(a1);
-  o.appendChild(y);
-
-
-  var a2=document.createElement("li");
-  a2.setAttribute("class","bb");
-  var a3=document.createElement("h3");
-  var a4=document.createTextNode("WORK");
-  a2.appendChild(a3);
-  a3.appendChild(a4);
-  o.appendChild(a2);
-
-
-  var a5=document.createElement("li");
-  a5.setAttribute("class","bb");
-  var a6=document.createElement("h3");
-  var a7=document.createTextNode("ABOUT US");
-  a5.appendChild(a6);
-  a6.appendChild(a7);
-  o.appendChild(a5);
-
-
-  var a8=document.createElement("li");
-  a8.setAttribute("class","bb");
-  var a9=document.createElement("h3");
-  var b1=document.createTextNode(" HOME");
-  a8.appendChild(a9);
-  a9.appendChild(b1);
-  o.appendChild(a8);
-
-
-
-var b3=document.createElement("div");
-b3.setAttribute("class","bb1");
-tab.appendChild(b3);
-
-
-
-var b2=document.createElement("img");
-b2.setAttribute("src","E:/sp/Board/slide6_wide1.jpg");
-b3.appendChild(b2);
-
-
-
-
-var aaa=document.createElement("section");
-document.body.appendChild(aaa);
-
-var a=document.createElement("div");
-	a.setAttribute("class","para");
-	aaa.appendChild(a);
-
-
-	var b=document.createElement("h2");
-	b.setAttribute("class","aa1");
-	var c=document.createTextNode("ABOUT US");
-
-	b.appendChild(c);
-
-	a.appendChild(b);
-		var d=document.createElement("hr");
-		d.setAttribute("class","aa2");
-		a.appendChild(d);
-
-
-
-		var e=document.createElement("p");
-		e.setAttribute("class","aa3");
-		var f=document.createTextNode("This is Photoshop's version of Lorem Ipsum ");
-		e.appendChild(f);
-		a.appendChild(e);
-
-
-		var g=document.createElement("p");
-		g.setAttribute("class","aa4");
-		var h=document.createTextNode("Proin gravida nibh vel velit auctor aliquet , Aenean sollicitudin, lorem quis bibendum aucto");
-		g.appendChild(h);
-		a.appendChild(g); 
-
-
-	   var gg=document.createElement("div");
-	   gg.setAttribute("class","tab");
-	   a.appendChild(gg);
-
-
-		var i=document.createElement("div");
-		i.setAttribute("class","aa5");
-		gg.appendChild(i);
-		var j=document.createElement("img");
-		j.setAttribute("src","E:/sp/Board/3.jpg");
-		j.setAttribute("class","aa6");
-		i.appendChild(j);
-
-
-		var ee=document.createElement("h2");
-		var ff=document.createTextNode("WHO WE ARE");
-		ee.appendChild(ff);
-		i.appendChild(ee);
-		var k=document.createElement("p");
-		k.setAttribute("class","aa7");
-		var l=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium eu mauris.");
-
-           k.appendChild(l);
-           i.appendChild(k);
-
-		
-	    var m=document.createElement("div");
-	    m.setAttribute("class","aa8");
-	    gg.appendChild(m);
-
-	    var n=document.createElement("img");
-	    n.setAttribute("src","E:/sp/Board/5.png");
-	    n.setAttribute("class","bb1");
-	    m.appendChild(n);
-
-
-	    var cc=document.createElement("h2");
-	    var dd=document.createTextNode("OUR GOAL");
-	    cc.appendChild(dd);
-	    m.appendChild(cc);
-
-	    var o=document.createElement("p");
-	    o.setAttribute("class","aa9");
-	    var p=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium eu mauris");
-		o.appendChild(p);
-		m.appendChild(o);
-
-
-		var q=document.createElement("div");
-		q.setAttribute("class","bb2");
-		gg.appendChild(q);
-		var r=document.createElement("img");
-		r.setAttribute("src","E:/sp/Board/7.png");
-		r.setAttribute("class","bb3");
-		q.appendChild(r);
-
-        var aa=document.createElement("h2");
-        var bb=document.createTextNode("OUR PHILOSOPHY");
-        aa.appendChild(bb);
-        q.appendChild(aa);
-       
-        var s=document.createElement("p");
-		s.setAttribute("class","bb4");
-		var t=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium eu mauris");
-         s.appendChild(t);
-         q.appendChild(s);
-
-
-
-
-         var u=document.createElement("hr");
-         u.setAttribute("class","bb5");
-         a.appendChild(u);
-
-
-
-         var v=document.createElement("div");
-         v.setAttribute("class","bb6");
-         a.appendChild(v);
-         var w=document.createElement("h2");
-         var x=document.createTextNode("OUR SERVICES");
-         w.appendChild(x);
-         v.appendChild(w);
-
-
-         var y=document.createElement("div");
-         y.setAttribute("class","bb7");
-         v.appendChild(y);
-         var z=document.createElement("h3");
-         var z1=document.createTextNode("CREATIVE IDEAS");
-
-         z.appendChild(z1);
-         y.appendChild(z);
-
-         var z2=document.createElement("hr");
-         y.appendChild(z2);
-
-         var z3=document.createElement("p");
-         z3.setAttribute("class","bb8");
-         var z4=document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mollis odio, vitae elementum sapien. Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium eu mauris. Duis vestibulum purus tempus, vestibulum purus sit amet, aliquam magna Sed ac mollis odio, vitae elementum sapien Aliquam vestibulum et erat eu elementum. Cras lacus massa, molestie sollicitudin pulvinar vitae, pretium"); 
-
-         z3.appendChild(z4);
-         y.appendChild(z3);
-
-         var z5=document.createElement("hr");
-         v.appendChild(z5);
-         var z7=document.createElement("div");
-         z7.setAttribute("class","bb9");
-         document.body.appendChild(z7);
-
-         var z5=document.createElement("h2");
-         var z6=document.createTextNode("MEET THE TEAM");
-         z5.appendChild(z6);
-         z7.appendChild(z5);
-
-         
 
 }
+    
+
+    
+
+
+
+	
